f4926df5046a047183685464d389f5555b3b590e
use-lie-die <tencent.lj@gmail.com>
2025-02-13 21:05:18 +0300

    erd + sql query

---
+++ src/main/index.js
@@ -0,0 +1,77 @@
+import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { join } from 'path'
+import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import icon from '../../resources/icon.png?asset'
+import pg from 'pg';
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 900,
+    height: 670,
+    show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === 'linux' ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false
+    }
+  })
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+    async function getPartners() {
+      const { Client } = pg;
+      const user = 'postgres';
+      const password = 'root';
+      const host = 'localhost';
+      const port = '5432';
+      const database = 'postgres';
+
+      const client = new Client({
+        user, password, host, port, database
+      });
+      await client.connect();
+      
+      try {
+        const response = await client.query(`SELECT T1.*`);
+        console.log(response.rows);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    getPartners();
+  })
+
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url)
+    return { action: 'deny' }
+  })
+
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  electronApp.setAppUserModelId('com.electron')
+
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
+
+  ipcMain.on('ping', () => console.log('pong'))
+
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
